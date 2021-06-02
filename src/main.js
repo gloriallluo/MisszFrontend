5a5da08fc0bc003ead12527c56e51de7f2394df4
luojj <jingjia_luo@163.com>
2021-06-02 20:12:06 +0800

    import and

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
+import Antd from 'ant-design-vue'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'element-ui/lib/theme-chalk/display.css'
+import 'ant-design-vue/dist/antd.css'
 import VueRouter from 'vue-router'
 
 import App from './App.vue'
@@ -11,6 +13,7 @@ import './registerServiceWorker'
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+Vue.use(Antd)
 Vue.use(VueRouter)
 
 new Vue({
