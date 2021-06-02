05791998ccf2fc87c7324b68427e29a58597aab7
luojj <jingjia_luo@163.com>
2021-06-02 20:28:40 +0800

    like/dislike function

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,8 @@
 const API = {
     POST_DREAM: '/backend/dream/',
-    GET_SQUARE: '/backend/dream/all/'
+    GET_SQUARE: '/backend/dream/all/',
+    POST_GOOD: '/backend/dream/good/',
+    POST_BAD: '/backend/dream/bad/'
 }
 
 export default API
